2b8b50aefae63eb8047567cc7ab863c27dadb28f
wilzon inturias <wilzoninturias@gmail.com>
2025-05-08 12:38:30 -0300

    inicio

---
+++ importadoswia/front/src/componentes/layout/Header.js
@@ -0,0 +1,6 @@
+const Header =(props) =>{
+    return (
+        <h2>hola soy el cabezal de mi sitio</h2>
+    )
+}
+export default Header;
---
+++ importadoswia/front/src/componentes/layout/Nav.js
@@ -0,0 +1,6 @@
+const Nav =(props) =>{
+    return (
+        <h2>hola soy la botonera del sitio</h2>
+    )
+}
+export default Nav;
